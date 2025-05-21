2cc86ab9f7cea606adbd0b86a56d4230ebb8dfa4
Gawesha <gawesharg21@gmail.com>
2025-05-21 14:10:32 +0530

    Add files via upload

---
+++ Day 1/OddEven.m
@@ -0,0 +1,7 @@
+num=10;
+
+if mod(num,2)==0
+    disp('The number is even number')
+else
+    disp('The number is odd number')
+end   
---
+++ Day 1/PositiveNegative.m
@@ -0,0 +1,8 @@
+num=4;
+
+if num>0
+    disp('The number is positive')
+else
+    disp('The numbers is negative')
+end
+
